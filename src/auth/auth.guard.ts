bb8fc61807944a960bdadf0fe7dfc59853539ff1
TradieNate <260296968+lifestyle3nergy-web@users.noreply.github.com>
2026-08-31 02:06:34 +1200

    refactor: use type-only Fastify imports

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,4 +1,4 @@
-import { FastifyRequest, FastifyReply } from 'fastify';
+import type { FastifyRequest, FastifyReply } from 'fastify';
 import { logger } from '@config';
 
 export async function authGuard(request: FastifyRequest, reply: FastifyReply) {
